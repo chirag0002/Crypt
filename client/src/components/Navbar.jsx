38ebe35bf7225795de2ef6e160d4be60a8825710
Chirag <varshneychirag34@gmail.com>
2022-07-18 15:36:29 +0530

    welcome page and navbar done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,6 +12,8 @@ const NavBarItem = ({ title, classprops }) => {
 
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
@@ -25,6 +27,25 @@ const Navbar = () => {
           Login
         </li>
       </ul> 
+      <div className="flex relative">
+        {!toggleMenu && (
+          <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+        )}
+        {toggleMenu && (
+          <ul
+            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+          >
+            <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
+            {["Market", "Exchange", "Tutorials", "Wallets"].map(
+              (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
+            )}
+          </ul>
+        )}
+      </div>
     </nav>
   );
 };
